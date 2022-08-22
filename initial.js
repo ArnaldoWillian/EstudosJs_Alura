c1f433741b5bd5bc5d102b4b172377aa43a66327
Arnaldo Willian <arnaldowillian2@gmail.com>
2022-08-22 17:30:28 -0300

    Aula01_JS

---
+++ initial.js
@@ -0,0 +1,28 @@
+// OBS: para rodar o JS é: node nomedoarquivo.js
+//bolean
+//true -> Verdadeiro
+//false = Falso
+
+//Obs: Quando se tem um sinal de "=" , está atribuindo um valor a variável e quando se tem dois ou 3 sinais de "=== " é uma comparação
+
+
+const texto1 = "Alura";
+const texto2= "alura";
+
+console.log(texto1 === texto2)
+
+/* 
+________________Numbers__________
+*/
+
+const n1 = 10;
+const n2 = 20;
+const soma = n1 + n2;
+//Operadores: + soma, - subtrai, * multiplica, / divide
+//numero inteiros: 1 , 2 , 3, 4....
+//Ponto flutuante ou numeros reais: 2.4 , 9.0, 8.5 .....
+console.log(soma)
+
+// NaN = Not a number ( não é um n°) , acontece se por exemplo a gente for fazer uma operação de uma string com um número("seja ele real ou inteiro").
+
+// Concatenação :  +  , usa-se o sinal de  " + "
