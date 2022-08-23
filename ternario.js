4d20715bef02e2b9208b9e8ff4b646ae5ae8240d
Arnaldo Willian <arnaldowillian2@gmail.com>
2022-08-23 17:09:54 -0300

    FimdoCurso

---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMin = 18;
+const idadeClie = 16;
+
+// if ( idadeClie >= idadeMin){
+//     console.log("Cerveja")
+// }else{
+//     console.log("Suco")
+// }
+
+
+console.log(idadeClie >= idadeMin ? "Cerveja" : "Suco")
