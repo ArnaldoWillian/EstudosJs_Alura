68e3f1dc2b88cf7b5838411c6521247c2bb1e8b0
Arnaldo Willian <arnaldowillian2@gmail.com>
2022-08-23 11:52:07 -0300

    Conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+//tipo de dados
+// booleanos
+
+// //Conversão implicita
+// const n1 = 465;
+// const numeroString = "465"; 
+// console.log (n1 + numeroString)
+
+// conversão explicita
+const n1 = 465;
+const numeroString = Number("465");
+console.log (n1 + numeroString)
--- initial.js
+++ initial.js
@@ -6,23 +6,40 @@
 //Obs: Quando se tem um sinal de "=" , está atribuindo um valor a variável e quando se tem dois ou 3 sinais de "=== " é uma comparação
 
 
-const texto1 = "Alura";
-const texto2= "alura";
+// const texto1 = "Alura";
+// const texto2= "alura";
 
-console.log(texto1 === texto2)
+// console.log(texto1 === texto2)
 
 /* 
 ________________Numbers__________
 */
 
-const n1 = 10;
-const n2 = 20;
-const soma = n1 + n2;
+//const n1 = 10;
+//const n2 = 20;
+//const soma = n1 + n2;
 //Operadores: + soma, - subtrai, * multiplica, / divide
 //numero inteiros: 1 , 2 , 3, 4....
 //Ponto flutuante ou numeros reais: 2.4 , 9.0, 8.5 .....
-console.log(soma)
+//console.log(soma)
 
 // NaN = Not a number ( não é um n°) , acontece se por exemplo a gente for fazer uma operação de uma string com um número("seja ele real ou inteiro").
 
 // Concatenação :  +  , usa-se o sinal de  " + "
+
+/*Tipos de Variaveis Js 
+Var => Pode ser alterada de qualquer parte do codigo
+Let => Tem que ser declarado no início do cod e não pode ser alterado
+Const => Não permite alterar a variável */
+
+//Boolean
+//const usuarioLogado = true;
+//const contapaga = false;
+
+// 0 => false
+// 1 => true
+
+console.log(0 === false)
+console.log("" == false)
+console.log(1 == true)
+
